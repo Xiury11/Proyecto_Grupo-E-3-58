ef70f833a596daf0357e01f3ef895addcd1463d0
Xiury11 <xiuryn1195@gmail.com>
2021-10-10 18:03:47 -0500

    Corrección de interfaz en VUE

--- vue-class/vue-app/src/App.vue
+++ vue-class/vue-app/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>  -->
+    <Landing> 
+
+    </Landing> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue';
+import Landing from './components/Landing.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld, 
+    Landing
   }
 }
 </script>
