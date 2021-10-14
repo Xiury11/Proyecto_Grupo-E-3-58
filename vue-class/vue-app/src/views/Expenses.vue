0a2a7a8252bd566dc6a64821502ec7e8793b74e6
Oscar Acevedo <oascaracegar07@gmail.com>
2021-10-14 19:27:43 +0200

    correcion registro

--- vue-class/vue-app/src/views/Expenses.vue
+++ vue-class/vue-app/src/views/Expenses.vue
@@ -23,13 +23,43 @@
         </header>
 
         <div class="row">
-
           <div class="col-lg-6">
             <h1>Ultimos gastos </h1>
-            <h2>Gasto 1</h2>
+            <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+            <!--<h2>Gasto 1</h2>
             <h2>Gasto 2</h2>
-            <h2>Gasto 3</h2>
+            <h2>Gasto 3</h2>-->
           </div>
+
+
 
           <div class="col-lg-6 ">
               <form action="forms/notify.php" method="post" role="form" class="php-email-form">
